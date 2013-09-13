d3598cbe5456d4c93667d674cbecfa52aaf067a9
Eric Haase <erichaase@gmail.com>
2013-09-12 19:30:05 -0500

    Added parsing of second HN page

--- lib/sync.rb
+++ lib/sync.rb
@@ -5,61 +5,63 @@ module Sync
 
   def Sync.process_page ( opts = {} )
     if opts[:path]
-      path = Rails.root.join('spec', 'lib', 'data', 'hacker_news', opts[:path]).to_s
+      paths = [Rails.root.join('spec', 'lib', 'data', 'hacker_news', opts[:path]).to_s]
     else
-      path = "https://news.ycombinator.com/news"
+      paths = ["https://news.ycombinator.com/news", "https://news.ycombinator.com/news2"]
     end
 
     # TODO add logging in production environment
-    # TODO add https://news.ycombinator.com/news2 , https://news.ycombinator.com/best and https://news.ycombinator.com/active
+    # TODO add https://news.ycombinator.com/best and https://news.ycombinator.com/active
 
-    doc = Nokogiri::HTML(open(path).read)
-    doc.css('table tr td table tr td.title a').each do |a|
-      data = lambda { |a|
-        # get td containing points and comments from a
-        tr = a.parent.parent.next
-        return nil if not tr
-        td = tr.css('td.subtext')
-        return nil if td.size != 1
-        td = td.first
+    paths.each do |path|
+      doc = Nokogiri::HTML(open(path).read)
+      doc.css('table tr td table tr td.title a').each do |a|
+        data = lambda { |a|
+          # get td containing points and comments from a
+          tr = a.parent.parent.next
+          return nil if not tr
+          td = tr.css('td.subtext')
+          return nil if td.size != 1
+          td = td.first
 
-        # get points from td
-        points = td.css('span')
-        return nil if points.size != 1
-        points = points.first.text[/^\d+/]
+          # get points from td
+          points = td.css('span')
+          return nil if points.size != 1
+          points = points.first.text[/^\d+/]
 
-        # get comments from td
-        comments = td.css('a')
-        return nil if comments.size != 2
-        comments = comments[1].text[/^\d+/]
+          # get comments from td
+          comments = td.css('a')
+          return nil if comments.size != 2
+          comments = comments[1].text[/^\d+/]
 
-        # TODO add debugging logging for a and td
+          # TODO add debugging logging for a and td
 
-        return :points => points, :comments => comments
-      }.call(a)
+          return :points => points, :comments => comments
+        }.call(a)
 
-      next if not data
+        next if not data
 
-      now = DateTime.now
-      attrs = {}
-      attrs[:title]     = a.text.strip
-      attrs[:url]       = a['href'].strip
-      attrs[:points]    = data[:points].to_i
-      attrs[:comments]  = data[:comments].to_i
-      attrs[:updated]   = now
+        now = DateTime.now
+        attrs = {}
+        attrs[:title]     = a.text.strip
+        attrs[:url]       = a['href'].strip
+        attrs[:points]    = data[:points].to_i
+        attrs[:comments]  = data[:comments].to_i
+        attrs[:updated]   = now
 
-      # TODO warn if duplicate db entries found
-      article = ArticleHN.where(:title => attrs[:title], :url => attrs[:url]).first
+        # TODO warn if duplicate db entries found
+        article = ArticleHN.where(:title => attrs[:title], :url => attrs[:url]).first
 
-      if article
-        status = article.update_attributes(attrs)
-      else
-        attrs[:published] = now
-        status = ArticleHN.create(attrs)
+        if article
+          status = article.update_attributes(attrs)
+        else
+          attrs[:published] = now
+          status = ArticleHN.create(attrs)
+        end
+
+        # TODO warn if validation fails (status == false)
+
       end
-
-      # TODO warn if validation fails (status == false)
-
     end
   end
 
